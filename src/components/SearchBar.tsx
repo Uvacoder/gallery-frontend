8bb704b60b284779bd6041d50eff5ab7001cb325
Sena Sarkaya <sena_sarkaya@hotmail.com>
2021-09-02 11:09:39 +0100

    search bar working

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -8,47 +8,22 @@ import {
 } from "@chakra-ui/react";
 
 function SearchBar(): JSX.Element {
-//   return (
-//     <>
-//       <Center>
-//         <Stack>
-//           <Heading
-//             color={useColorModeValue("gray.700", "white")}
-//             fontSize={"2xl"}
-//             fontFamily={"body"}
-//           >
-//             Rediscover the Arts
-//           </Heading>
-//           <Text color={"gray.500"} noOfLines={6}>
-//             Discover inspiring art, artists and stories in the digitised
-//             collections of European museums, galleries, ibraries and archives.
-//             Explore paintings, drawings, engravings and sculpture from cultural
-//             heritage institutions across Europe.
-//           </Text>
-//         </Stack>
-//       </Center>
-//       <Input placeholder="Search your favourite artist or media" />
-//       <Button colorScheme="teal" variant="outline">
-//         Search
-//       </Button>
-//     </>
-//   );
 
     return (
     <div>
-     <Stack spacing={8}>
+     <Stack spacing={8} mt={2}>
        <Box p={10} shadow="md" borderWidth="0.5px" borderRadius="lg" bgColor={"gray.50"}>
         <Heading fontSize="xx-large" textAlign='center'>Rediscover the Arts</Heading>
         <Text mt={4} color={"gray.600"} textAlign='center'>
-                Discover inspiring art, artists and stories in the digitised
+            Discover inspiring art, artists and stories in the digitised
             collections of European museums, galleries, ibraries and archives.
             Explore paintings, drawings, engravings and sculpture from cultural
-             heritage institutions across Europe.
-             </Text>
+            heritage institutions across Europe.
+        </Text>
         </Box>
       </Stack>
-      <Input placeholder="Search your favourite artist or media" />
-      <Button colorScheme="teal" variant="outline" paddingStart="2">
+      <Input placeholder="Search your favourite artist or media" maxW={"lg"} mt={5} alignContent={'center'}/>
+      <Button colorScheme="teal" variant="outline" paddingStart="4" maxW={"lg"}>
          Search
        </Button>
     </div> 
@@ -56,8 +31,3 @@ function SearchBar(): JSX.Element {
 }
 
 export default SearchBar;
-// p={5}
-// shadow="md"
-// borderWidth="1px"
-// flex="1"
-// borderRadius="md"
