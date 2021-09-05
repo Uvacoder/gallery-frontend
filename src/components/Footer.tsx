4aa1864ad9966f20af2939f234a94cc6277612bb
Sena Sarkaya <sena_sarkaya@hotmail.com>
2021-09-05 13:01:41 +0100

    footer changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -83,7 +83,7 @@ export default function Footer(): JSX.Element {
           justify={{ base: "center", md: "space-between" }}
           align={{ base: "center", md: "center" }}
         >
-          <Text>© 2020 Chakra Templates. All rights reserved</Text>
+          <Text>© 2021 All rights reserved</Text>
           <Stack direction={"row"} spacing={6}>
             <SocialButton
               label={"Twitter"}
